a01fe00b377b2eb27a6612152209b5391a70a498
tabithacarnes <tabithacarnes@rocketmail.com>
2017-11-06 16:52:38 -0500

    changed cards to carousel, also had javascript for the carousel, and added navigation to index.html

--- javascript/apps.js
+++ javascript/apps.js
@@ -1,3 +1,7 @@
+/*Alert Box*/
 function myFunction() {
     alert("Your Question Has Been Submitted! You should recieve a response within 48 hours!");
 }
+
+/*Carousal*/
+$('.carousel').carousel('pause');
